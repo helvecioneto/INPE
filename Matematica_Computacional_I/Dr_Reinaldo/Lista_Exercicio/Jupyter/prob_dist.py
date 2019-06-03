9032b3c4619e2ae5ff97ed5e5ce5bf130cf464b1
Helvecio Neto <helvecioneto@inpe.br>
2019-06-03 12:46:54 -0300

    Lista Reinaldo

--- Matematica_Computacional_I/Dr_Reinaldo/Lista_Exercicio/Jupyter/prob_dist.py
+++ Matematica_Computacional_I/Dr_Reinaldo/Lista_Exercicio/Jupyter/prob_dist.py
@@ -16,9 +16,9 @@ def uniforme():
 	sns.distplot(uniform100,hist=False, color='green',label='N=100')
 	sns.distplot(uniform10, hist=False, color="black", label='N=100')
 
-	plt.xlabel('Variáveis Aleatórias')
-	plt.ylabel('Probabilidade')
-	plt.title('(i)Uniforme')
+	plt.xlabel('Variáveis Aleatórias',fontsize=16)
+	plt.ylabel('Probabilidade',fontsize=16)
+	plt.title('(i)Uniforme N=10000',fontsize=16)
 
 def binomial():
 	binomial1 = np.random.binomial(100,.15, 1000)
@@ -33,9 +33,9 @@ def binomial():
 	sns.distplot(binomial3,hist=False, color='green',label='n=100 p=0.65')
 	sns.distplot(binomial4,hist=False, color='black',label='n=100 p=0.85')
 
-	plt.xlabel('Variáveis Aleatorias')
-	plt.ylabel('Probabilidade')
-	plt.title('(ii)Binomial')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(ii)Binomial N=10000',fontsize=16)
 
 def beta():
 	beta1 = np.random.beta(2, 8, 1000)  #Alpha = 2, Beta = 8
@@ -44,13 +44,13 @@ def beta():
 
 	plt.figure(figsize=(12, 4))
 
-	sns.distplot(beta1,hist=False, color='blue',label='alpha=2')
-	sns.distplot(beta2,hist=False, color='red',label='alpha=4')
-	sns.distplot(beta3,hist=False, color='green',label='alpha=6')
+	sns.distplot(beta1,hist=False, color='blue',label='α=2 β = 8')
+	sns.distplot(beta2,hist=False, color='red',label='α=4 β = 8')
+	sns.distplot(beta3,hist=False, color='green',label='α=6 β = 8')
 
-	plt.xlabel('Beta = 8')
-	plt.ylabel('Probabilidade')
-	plt.title('(iii)Beta')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(iii)Beta N=10000',fontsize=16)
 
 def laplace():
 	laplace1 = np.random.laplace(0, .1, 1000)	#Loc , Scale, Size
@@ -65,9 +65,9 @@ def laplace():
 	sns.distplot(laplace3,hist=False, color='green',label='loc=0 b=4')
 	sns.distplot(laplace4,hist=False, color='black',label='loc=5 b=4')
 
-	plt.xlabel('Variáveis Aleatorias')
-	plt.ylabel('Probabilidade')
-	plt.title('(iv)LaPlace')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(iv)LaPlace N=10000',fontsize=16)
 
 def gamma():
 	gamma1 = np.random.gamma(1, 2, 1000)	#Shape, Scale, Size
@@ -84,9 +84,9 @@ def gamma():
 	sns.distplot(gamma4,hist=False, color='black',label='α=5 θ=1')
 	sns.distplot(gamma5,hist=False, color='yellow',label='α=9 θ=0.5')
 
-	plt.xlabel('Variáveis Aleatorias')
-	plt.ylabel('Probabilidade')
-	plt.title('(v)Gamma')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(v)Gamma N=10000',fontsize=16)
 
 def exponencial():
 	exponential1 = np.random.exponential(0.5, 1000)	#Scale
@@ -102,9 +102,9 @@ def exponencial():
 	sns.distplot(exponential3,hist=False, color='green',label='λ=2')
 	sns.distplot(exponential4,hist=False, color='black',label='λ=4')
 
-	plt.xlabel('Variáveis Aleatorias')
-	plt.ylabel('Probabilidade')
-	plt.title('(vi)Exponencial')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(vi)Exponencial N=10000',fontsize=16)
 
 def quiquadrado():
 	quiquadrado1 = np.random.chisquare(1, 1000)		#k graus
@@ -121,9 +121,9 @@ def quiquadrado():
 	sns.distplot(quiquadrado4,hist=False, color='black',label='k=4')
 	sns.distplot(quiquadrado5,hist=False, color='black',label='k=9')
 
-	plt.xlabel('Variáveis Aleatorias')
-	plt.ylabel('Probabilidade')
-	plt.title('(vi)Qui-Quadrado')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(vi)Qui-Quadrado N=10000',fontsize=16)
 
 def cauchy():
 	cauchy1 = np.random.standard_cauchy(10)
@@ -135,12 +135,43 @@ def cauchy():
 
 	sns.distplot(cauchy1,hist=False, color='blue',label='n=10')
 	sns.distplot(cauchy2,hist=False, color='red',label='n=100')
-	sns.distplot(cauchy3,hist=False, color='blue',label='n=1000')
-	sns.distplot(cauchy4,hist=False, color='red',label='n=10000')
-	#sns.distplot(quiquadrado3,hist=False, color='green',label='k=3')
-	#sns.distplot(quiquadrado4,hist=False, color='black',label='k=4')
-	#sns.distplot(quiquadrado5,hist=False, color='black',label='k=9')
+	sns.distplot(cauchy3,hist=False, color='black',label='n=1000')
+	sns.distplot(cauchy4,hist=False, color='orange',label='n=10000')
 
-	plt.xlabel('Variáveis Aleatorias')
-	plt.ylabel('Probabilidade')
-	plt.title('(vii)Cauchy')
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(vii)Cauchy N=10000',fontsize=16)
+
+def beta_():
+	beta1_ = np.random.beta(0.5, 0.5, 1000)  #Alpha = 2, Beta = 8
+	beta2_ = np.random.beta(5, 1, 1000)
+	beta3_ = np.random.beta(1, 3, 1000)
+	beta4_ = np.random.beta(2, 2, 1000)
+	beta5_ = np.random.beta(2, 5, 1000)
+
+	plt.figure(figsize=(12, 4))
+
+	sns.distplot(beta1_,hist=False, color='blue',label='α= β = 0.5')
+	sns.distplot(beta2_,hist=False, color='red',label='α=5 β = 1')
+	sns.distplot(beta3_,hist=False, color='green',label='α=1 β = 3')
+	sns.distplot(beta4_,hist=False, color='orange',label='α=2 β = 2')
+	sns.distplot(beta5_,hist=False, color='black',label='α=2 β = 5')
+
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(vii)Beta N=10000',fontsize=16)
+
+def gaussian_normal():
+	gaussian_normal1 = np.random.normal(0, 0.5, 1000)
+	gaussian_normal2 = np.random.normal(0, 1, 1000)
+	gaussian_normal3 = np.random.normal(0, 2, 1000)
+
+	plt.figure(figsize=(12, 4))
+
+	sns.distplot(gaussian_normal1,hist=False, color='blue',label='μ= 0 σ = 0.5')
+	sns.distplot(gaussian_normal2,hist=False, color='red',label='μ=0 σ = 1')
+	sns.distplot(gaussian_normal3,hist=False, color='green',label='μ=0 σ = 2')
+
+	plt.xlabel('Variáveis Aleatorias',fontsize=14)
+	plt.ylabel('Probabilidade',fontsize=14)
+	plt.title('(viii)Gaussian Normal N=10000',fontsize=16)
